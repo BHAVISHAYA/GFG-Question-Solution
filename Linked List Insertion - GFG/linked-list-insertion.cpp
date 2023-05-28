842abc9013e7f00d8c04a84a98eb910764347dde
Bhavishaya Khandelwal <99893459+BHAVISHAYA@users.noreply.github.com>
2023-05-28 22:16:14 +0530

    Added solution - LeetHub

--- Linked List Insertion - GFG/linked-list-insertion.cpp
+++ Linked List Insertion - GFG/linked-list-insertion.cpp
@@ -39,30 +39,35 @@ class Solution{
     //Function to insert a node at the beginning of the linked list.
     Node *insertAtBegining(Node *head, int x) {
        // Your code here
-       Node* newNode = new Node(x);
-       newNode -> next = head;
-       head = newNode;
+       if(head == NULL) {
+           Node* newNode = new Node(x);
+           head = newNode;
+       }
+       else {
+           Node* newNode = new Node(x);
+           newNode -> next = head;
+           head = newNode;
+       }
        return head;
     }
     
     
     //Function to insert a node at the end of the linked list.
     Node *insertAtEnd(Node *head, int x)  {
-        if(head == NULL) {
-            Node* newNode = new Node(x);
-            return newNode;
-        }
-        else {
-            Node* temp = head;
-            while(temp -> next != NULL) {
-                temp = temp -> next;
-            }
-            Node* tail = temp;
-            Node* newNode = new Node(x);
-            tail -> next = newNode;
-            tail = newNode;
-            return head;
-        }
+       // Your code here
+       if(head == NULL) {
+           Node* newNode = new Node(x);
+           head = newNode;
+       }
+       else {
+           Node* temp = head;
+           while(temp -> next != NULL) {
+               temp = temp -> next;
+           }
+           Node* newNode = new Node(x);
+           temp -> next = newNode;
+       }
+       return head;
     }
 };
 
