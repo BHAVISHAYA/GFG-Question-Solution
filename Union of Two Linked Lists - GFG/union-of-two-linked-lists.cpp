0eaadffd0c9e0f2e40fa1358a07f91d990739492
Bhavishaya Khandelwal <99893459+BHAVISHAYA@users.noreply.github.com>
2023-06-27 07:05:00 +0530

    Added solution - LeetHub

--- Union of Two Linked Lists - GFG/union-of-two-linked-lists.cpp
+++ Union of Two Linked Lists - GFG/union-of-two-linked-lists.cpp
@@ -14,6 +14,65 @@ struct Node
 	
 };
 
+
+// } Driver Code Ends
+/*
+// structure of the node is as follows
+
+struct Node
+{
+	int data;
+	struct Node* next;
+	
+	Node(int x){
+	    data = x;
+	    next = NULL;
+	}
+	
+};
+
+*/
+class Solution
+{
+    public:
+    void insertAtTail(struct Node* &head, struct Node* &tail, int data) {
+        if(head == NULL) {
+            struct Node* newNode = new Node(data);
+            head = newNode;
+            tail = newNode;
+        }
+        else {
+            struct Node* newNode = new Node(data);
+            tail -> next = newNode;
+            tail = newNode;
+        }
+    }
+    struct Node* makeUnion(struct Node* head1, struct Node* head2)
+    {
+        // code 
+        set<int> st;
+        struct Node* temp = head1;
+        struct Node* tmp = head2;
+        struct Node* HEAD = NULL;
+        struct Node* TAIL = NULL;
+        while(temp != NULL) {
+            st.insert(temp -> data);
+            temp = temp -> next;
+        }
+        while(tmp != NULL) {
+            st.insert(tmp -> data);
+            tmp = tmp -> next;
+        }
+        for(auto &value : st) {
+            insertAtTail(HEAD, TAIL, value);
+        }
+        return HEAD;
+    }
+};
+
+
+//{ Driver Code Starts.
+
 struct Node* buildList(int size)
 {
     int val;
@@ -42,7 +101,6 @@ void printList(Node* n)
     cout<< endl;
 }
 
-struct Node* makeUnion(struct Node* head1, struct Node* head2);
 
 int main()
 {
@@ -57,61 +115,11 @@ int main()
         
         cin>>m;
         Node* second = buildList(m);
-        
-        printList(makeUnion(first,second));
+        Solution obj;
+        Node* head = obj.makeUnion(first,second);
+        printList(head);
     }
     return 0;
 }
 
 // } Driver Code Ends
-
-
-/*
-// structure of the node is as follows
-
-struct node
-{
-	int data;
-	struct node* next;
-	
-	node(int x){
-	    data = x;
-	    next = NULL;
-	}
-	
-};
-
-*/
-
-void insertAtTail(struct Node* &tail, struct Node* &head, int data) {
-    if(tail == NULL) {
-        struct Node* newNode = new Node(data);
-        tail = newNode;
-        head = newNode;
-    }
-    else {
-        struct Node* newNode = new Node(data);
-        tail -> next = newNode;
-        tail = newNode;
-    }
-}
-
-struct Node* makeUnion(struct Node* head1, struct Node* head2)
-{
-    // code here
-    struct Node* tail = NULL;
-    struct Node* head = NULL;
-    set<int> st;
-    while(head1 != NULL) {
-        st.insert(head1 -> data);
-        head1 = head1 -> next;
-    }
-    while(head2 != NULL) {
-        st.insert(head2 -> data);
-        head2 = head2 -> next;
-    }
-    for(auto &value : st) {
-        insertAtTail(tail, head, value);
-    }
-    return head;
-}
