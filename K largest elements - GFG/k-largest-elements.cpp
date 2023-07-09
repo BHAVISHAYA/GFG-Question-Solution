3d12167bb9115f590a89fa2fe987e9fcfd9ba26b
Bhavishaya Khandelwal <99893459+BHAVISHAYA@users.noreply.github.com>
2023-07-09 08:13:31 +0530

    Added solution - LeetHub

---
+++ K largest elements - GFG/k-largest-elements.cpp
@@ -0,0 +1,54 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution
+{
+    public:
+    //Function to return k largest elements from an array.
+    vector<int> kLargest(int arr[], int n, int k)
+    {
+        // code here
+        priority_queue<int, vector<int>, greater<int>> minH;
+        for(int i = 0 ; i < n ; i++) {
+            minH.push(arr[i]);
+            if(minH.size() > k) 
+            minH.pop();
+        }
+        vector<int> v(minH.size());
+        int i = minH.size() - 1;
+        while(!minH.empty()) {
+            v[i] = minH.top();
+            minH.pop();
+            i--;
+        }
+        return v;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main(){
+    int t;
+    cin >> t;
+    while(t--){
+        int n, k;
+        cin >> n >> k;
+        
+        int arr[n];
+        for(int i = 0; i < n;i++)
+            cin>>arr[i];
+        Solution ob;
+        vector<int> result = ob.kLargest(arr, n, k);
+        for (int i = 0; i < result.size(); ++i)
+            cout<<result[i]<<" ";
+        cout << endl;
+        
+    }
+    return 0;
+}
+
+// } Driver Code Ends
