14a6af6534003de7cc92fe02212a9b72093e48bb
Bhavishaya Khandelwal <99893459+BHAVISHAYA@users.noreply.github.com>
2023-07-23 21:44:51 +0530

    Added solution - LeetHub

--- Given a linked list of 0s, 1s and 2s, sort it. - GFG/given-a-linked-list-of-0s-1s-and-2s-sort-it..cpp
+++ Given a linked list of 0s, 1s and 2s, sort it. - GFG/given-a-linked-list-of-0s-1s-and-2s-sort-it..cpp
@@ -33,56 +33,34 @@ class Solution
 {
     public:
     //Function to sort a linked list of 0s, 1s and 2s.
-    
-    void insertAtTail(Node* &tail, int data) {
+    Node* insert(Node* &head, Node* &tail, int data) {
         Node* newNode = new Node(data);
-        tail -> next = newNode;
-        tail = newNode;
-    }
-    Node* segregate(Node *head) {
-        
-        // Add code here
-        if(head == NULL && head -> next == NULL) return head;
-        
-        Node* zeroHead = new Node(-1);
-        Node* zeroTail = zeroHead;
-        Node* oneHead = new Node(-1);
-        Node* oneTail = oneHead;
-        Node* twoHead = new Node(-1);
-        Node* twoTail = twoHead;
-        
-        Node* curr = head;
-        while(curr != NULL) {
-            int value = curr -> data;
-            if(value == 0) 
-            insertAtTail(zeroTail, value);
-            else if(value == 1) 
-            insertAtTail(oneTail, value);
-            else 
-            insertAtTail(twoTail, value);
-            
-            curr = curr -> next;
-        }
-        
-        if(oneHead -> next == NULL) {
-            zeroTail -> next = twoHead -> next;
+        if(head == NULL) {
+            head = newNode;
+            tail = newNode;
         }
         else {
-            zeroTail -> next = oneHead -> next;
+            tail -> next = newNode;
+            tail = newNode;
         }
-        oneTail -> next = twoHead -> next;
-        twoTail -> next = NULL;
-        
-        // Set head element
-        head = zeroHead -> next;
-        
-        
-        // Now delete all the dummy nodes;
-        delete oneHead;
-        delete zeroHead;
-        delete twoHead;
-        
         return head;
+    }
+    Node* segregate(Node *head) {
+        map<int ,int> m;
+        Node* temp = head;
+        while(temp != NULL) {
+            m[temp -> data]++;
+            temp = temp -> next;
+        }
+        Node* Head = NULL;
+        Node* Tail = NULL;
+        for(auto &value : m) {
+            int t = value.second;
+            while(t--) {
+                Head = insert(Head, Tail, value.first);
+            }
+        }
+        return Head;
     }
 };
 
